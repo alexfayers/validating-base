80dc73dfb4a14ccfd432ad80e2a314776c942684
Alex Fayers <alex.fayers@falanx.com>
2023-04-24 09:17:54 +0100

    fix mypy typing

--- src/validating_base/base.py
+++ src/validating_base/base.py
@@ -81,8 +81,8 @@ class ValidatingBaseClassMeta(type):
                 Returns:
                     Any: The return value of the decorated method
                 """
-                typechecked(validate_method)(*args, **kwargs)
-                return typechecked(method)(*args, **kwargs)
+                typechecked(validate_method)(*args, **kwargs)  # type: ignore[type-var,misc]
+                return typechecked(method)(*args, **kwargs)  # type: ignore[type-var,misc]
 
             setattr(new_class, method_name, _validated)
 
